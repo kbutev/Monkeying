992106b4ac130b75f565f0ec92b9a47cc81417ac
Kristiyan <felixisto@outlook.com>
2024-05-26 23:29:34 +0300

    Create scripts folder if not created already

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 # Python 3
+import os
+
 from PyQt5.QtWidgets import QApplication
 import sys
 from MainView.MainRouter import MainRouter
 from Service.Dependencies import DependencyService
 
+SCRIPTS_PATH = 'scripts'
+
 # DI
 DependencyService.setup()
+
+# Scripts folder
+if not os.path.exists(SCRIPTS_PATH):
+    os.makedirs(SCRIPTS_PATH)
 
 # Primary setup
 router = MainRouter()
